ef2b338189f55e92d542c4fbac57d65fa71dbddd
Thomas Womack <tom@womack.net>
2025-03-02 15:00:34 +0000

    Handle malformed input file without assertion failure; fix off-by-one error that was crashing the output phase

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,15 @@ use rayon::prelude::*;
 
 use bitvec::prelude::*;
 
+// need an error type
+#[derive(Debug, Clone)]
+struct ParseError;
+
+impl ToString for ParseError
+{
+  fn to_string(&self) -> String { "Parsing error".to_string() }
+}
+
 #[derive(Debug, Parser)]
 #[clap(name = "philtre", version = "0.0.0", author = "Tom Womack")]
 pub struct PhiltreCmdLine {
@@ -70,13 +79,14 @@ fn find_fast_byte_after(start: &[u8], target: u8) -> usize {
     return start.len();
 }
 
-fn fast_read_unsigned(number: &[u8]) -> u64 {
+fn fast_read_unsigned(number: &[u8]) -> Result<u64, ParseError> {
     if number.len() > 16 {
         println!("Unexpectedly long integer of {} characters", number.len());
         for i in 0..number.len() {
             println!("{} {}", number[i] as char, number[i]);
         }
-        assert!(number.len() <= 16);
+        return Err(ParseError);
+        //        assert!(number.len() <= 16);
     }
 
     let mut k: u64 = 0;
@@ -86,24 +96,26 @@ fn fast_read_unsigned(number: &[u8]) -> u64 {
         k = k + M * ((number[L - r] - 48) as u64);
         M = M * 10
     }
-    k
+    Ok(k)
 }
 
-fn fast_read_signed(number: &[u8]) -> i64 {
+fn fast_read_signed(number: &[u8]) -> Result<i64, ParseError> {
     if number[0] == b'-' {
-        -(fast_read_unsigned(&number[1..]) as i64)
+       let n = fast_read_unsigned(&number[1..])?;
+       Ok(-(n as i64))
     } else {
-        fast_read_unsigned(number) as i64
+        let n = fast_read_unsigned(number)?;
+        Ok(n as i64)
     }
 }
 
-fn fast_read_xy(xy: &[u8]) -> SieveIndex {
+fn fast_read_xy(xy: &[u8]) -> Result<SieveIndex, ParseError> {
     let colon = find_fast_byte_after(xy, b':');
     let comma = find_fast_byte_after(xy, b',');
     //  println!("{} {} {}  {} {} {}", xy[colon-1], xy[colon], xy[colon+1], xy[comma-1], xy[comma], xy[comma+1]);
     let xx = fast_read_signed(&xy[0..comma]);
-    let yy = fast_read_unsigned(&xy[comma + 1..colon]) as u32;
-    return SieveIndex { x: xx, y: yy };
+    let yy = fast_read_unsigned(&xy[comma + 1..colon]);
+    Ok(SieveIndex { x: xx?, y: (yy? as u32) })
 }
 
 // note that Chunk.line_starts has *one more entry than the number of lines*
@@ -114,14 +126,13 @@ fn fast_read_xy(xy: &[u8]) -> SieveIndex {
 // line_valid.size()
 
 impl Chunk<'_> {
-    pub fn identify_lines(&mut self, data: &[u8]) -> usize {
-        let chunk = &data[self.start_ix..self.end_ix];
+    pub fn identify_lines(&mut self) -> usize {
         let mut nlines: usize = 0;
         let mut ptr: usize = 0;
-        let L = chunk.len();
+        let L = self.chunk.len();
         while ptr < L {
             self.line_starts.push(ptr);
-            let eol = find_fast_byte_after(&chunk[ptr..], b'\n');
+            let eol = find_fast_byte_after(&self.chunk[ptr..], b'\n');
             nlines = 1 + nlines;
             ptr = ptr + 1 + eol;
         }
@@ -130,8 +141,6 @@ impl Chunk<'_> {
         self.line_valid.resize(nlines, true);
         nlines
     }
-
-    // there are 2015110 comment lines in finish-9282-1472/msieve.dat
 
     pub fn invalidate_comments(&mut self) -> usize {
         let mut ncomments: usize = 0;
@@ -154,42 +163,82 @@ impl Chunk<'_> {
         vl
     }
 
-    pub fn write_out(&self, data: &[u8], dest_m: &Mutex<&mut [u8]>) {
+    pub fn write_out(&self, dest_m: &Mutex<&mut [u8]>) -> usize {
         let mut dest = (*dest_m).lock().unwrap();
-        let mut ptr: usize = 0;
-        for i in 0..self.line_starts.len() - 1 {
+        println!(
+            "Writing out from chunk {}..={} to dest slice of size {}",
+            self.start_ix,
+            self.end_ix,
+            dest.len()
+        );
+        println!(
+            "Chunk size is {}, last two self.line_starts are {} {}",
+            self.chunk.len(),
+            self.line_starts[self.line_starts.len() - 2],
+            self.line_starts[self.line_starts.len() - 1]
+        );
+	println!("Last three bytes of my src are 0x{:02x} 0x{:02x} 0x{:02x}", self.chunk[self.chunk.len()-3],
+	self.chunk[self.chunk.len()-2],
+	self.chunk[self.chunk.len()-1]);
+
+	let mut ptr: usize = 0;
+        for i in 0..=self.line_starts.len() - 2 {
             if self.line_valid[i] {
-                let line_length = self.line_starts[i + 1] - self.line_starts[i];
-                (&mut dest[ptr..ptr + line_length])
-                    .copy_from_slice(&data[self.line_starts[i]..self.line_starts[i + 1]]);
+                let mut line_length = self.line_starts[i + 1] - self.line_starts[i];
+		if (i == self.line_starts.len()-1) { line_length -= 1; }
+		if ((ptr+line_length) as i64 - dest.len() as i64).abs() < 100
+		{
+		  println!("Calling memcpy on my line {} (I have {} lines); src {}+{} dest {}+{} src_len {} dest_len {}", i, self.line_starts.len(), self.line_starts[i], self.line_starts[i]+line_length, ptr, ptr+line_length, self.chunk.len(), dest.len());
+		}
+                (&mut dest[ptr..ptr + line_length]).copy_from_slice(
+                    &self.chunk[self.line_starts[i]..self.line_starts[i] + line_length],
+                );
                 ptr += line_length;
             }
         }
+        ptr
     }
 
     pub fn sharded_read(
         &self,
         shards: &RwLock<[Mutex<HashMap<SieveIndex, usize>>]>,
         sharding_prime: usize,
-    ) {
+    ) -> (usize, Vec<usize>) {
+        let mut lines_read = 0;
+	let mut bad_line_vector = Vec::new();
         for i in 0..=(self.end_line - self.start_line) {
             if self.line_valid[i] {
+                lines_read += 1;
                 let current_line = self.start_line + i;
-                let xy = fast_read_xy(&self.chunk[self.line_starts[i]..]);
-                let shard: usize = (xy.x.rem_euclid(sharding_prime as i64)) as usize
-                    + sharding_prime * (xy.y.rem_euclid(sharding_prime as u32) as usize)
-                    - 1;
-                let shards_reader = shards.read().unwrap();
-                let shard_mutex = shards_reader.get(shard).unwrap();
-                // block where we actually need to do something locked
-                {
-                    let mut data = shard_mutex.lock().unwrap();
-                    if !data.contains_key(&xy) || data[&xy] > current_line {
-                        data.insert(xy, current_line);
+                let xy_fallible = fast_read_xy(&self.chunk[self.line_starts[i]..]);
+                // this is the first time we've read the line, it's totally possible there's an error
+                // in which case we mark the line as invalid and carry on
+                match xy_fallible {
+                    Err(err) => {
+                        println!("Problem parsing line {}: {}", i + self.start_line, err.to_string());
+			// because we're doing this without being able to modify self,
+			// we can't do "self.line_valid.set(i, false);"
+			// instead we record the bad line for a later serial marking pass
+                        bad_line_vector.push(i);
+                    }
+                    Ok(xy) => {
+                        let shard: usize = (xy.x.rem_euclid(sharding_prime as i64)) as usize
+                            + sharding_prime * (xy.y.rem_euclid(sharding_prime as u32) as usize)
+                            - 1;
+                        let shards_reader = shards.read().unwrap();
+                        let shard_mutex = shards_reader.get(shard).unwrap();
+                        // block where we actually need to do something locked
+                        {
+                            let mut data = shard_mutex.lock().unwrap();
+                            if !data.contains_key(&xy) || data[&xy] > current_line {
+                                data.insert(xy, current_line);
+                            }
+                        }
                     }
                 }
             }
         }
+        (lines_read, bad_line_vector)
     }
 
     pub fn mark_dupes(
@@ -197,12 +246,13 @@ impl Chunk<'_> {
         shards: &RwLock<[Mutex<HashMap<SieveIndex, usize>>]>,
         sharding_prime: usize,
     ) -> usize {
-	let mut duplicates = 0;
-	for i in 0..self.line_starts.len()
-	{
-	    if self.line_valid[i]
-	    {
-                let xy = fast_read_xy(&self.chunk[self.line_starts[i]..]);
+        let mut duplicates = 0;
+        for i in 0..self.line_valid.len() {
+            if self.line_valid[i] {
+	        // by the time we're calling this function, we have already done fast_read_xy on
+		// every line in the file and marked unreadable ones as invalid,
+		// so we can unwrap() and the panic if it's unreadable is a logic error
+                let xy = fast_read_xy(&self.chunk[self.line_starts[i]..]).unwrap();
                 let shard: usize = (xy.x.rem_euclid(sharding_prime as i64)) as usize
                     + sharding_prime * (xy.y.rem_euclid(sharding_prime as u32) as usize)
                     - 1;
@@ -212,7 +262,7 @@ impl Chunk<'_> {
                 // because it's read-only access
                 {
                     let data = shard_mutex.lock().unwrap();
-		    let current_line = self.start_line + i;
+                    let current_line = self.start_line + i;
                     if data[&xy] != current_line {
                         self.line_valid.set(i, false);
                         duplicates += 1;
@@ -224,7 +274,7 @@ impl Chunk<'_> {
     }
 }
 
-fn emit_uncancelled_lines(output_filename: String, v: &[Chunk], mmap_r: &[u8]) -> io::Result<()> {
+fn emit_uncancelled_lines(output_filename: String, v: &[Chunk]) -> io::Result<()> {
     // Get the sum of the lengths of the uncancelled parts of the chunks
     let n = v.len();
     let valid_lengths: Vec<usize> = v.par_iter().map(|c| c.valid_length()).collect();
@@ -238,8 +288,15 @@ fn emit_uncancelled_lines(output_filename: String, v: &[Chunk], mmap_r: &[u8]) -
     let total_length: usize = sp;
 
     // And actually perform the writes
-    let ofile = File::create(output_filename).unwrap();
-    ofile.set_len(total_length as u64)?;
+    {
+        let ofile1 = File::create(output_filename.clone()).unwrap();
+        ofile1.set_len(total_length as u64)?;
+    }
+    // unfortunately ofile1 is opened write-only by ::create and mmap requires RdWr
+    let ofile = File::options()
+        .read(true)
+        .write(true)
+        .open(output_filename)?;
     let mut mmap_w = unsafe { MmapOptions::new().map_mut(&ofile).unwrap() };
 
     // Use split_at_mut to let us pass the segments of the file to the iterator in parallel
@@ -248,12 +305,15 @@ fn emit_uncancelled_lines(output_filename: String, v: &[Chunk], mmap_r: &[u8]) -
     for i in 0..n {
         let (seg, rest2) = rest.split_at_mut(valid_lengths[i]);
         segments.push(Mutex::new(seg));
+        println!("Segment {} created with size {}", i, valid_lengths[i]);
         rest = rest2;
     }
 
-    let _ = (0..n)
+    let out_bytes: usize = (0..n)
         .into_par_iter()
-        .map(|i| v[i].write_out(&mmap_r, &segments[i]));
+        .map(|i| v[i].write_out(&segments[i]))
+        .sum();
+    println!("Wrote out {} bytes", out_bytes);
     mmap_w.flush()?;
     Ok(())
 }
@@ -287,7 +347,7 @@ fn main() {
         v[a].start_ix = st + wug + 1;
     }
     for a in &mut v {
-        a.chunk = &mmap[a.start_ix..a.end_ix];
+        a.chunk = &mmap[a.start_ix..=a.end_ix];
     }
 
     const n_shards: usize = sharding_prime * sharding_prime - 1;
@@ -296,7 +356,7 @@ fn main() {
     let xys_under_rwlock: RwLock<_> = RwLock::new(xys);
 
     // count the lines (needed so each chunk knows where it starts)
-    let lines_per_chunk: Vec<usize> = v.par_iter_mut().map(|a| a.identify_lines(&mmap)).collect();
+    let lines_per_chunk: Vec<usize> = v.par_iter_mut().map(|a| a.identify_lines()).collect();
     println!("{} lines in file", lines_per_chunk.iter().sum::<usize>());
 
     let mut nx: usize = 0;
@@ -320,19 +380,10 @@ fn main() {
             t += 1 + find_fast_byte_after(&mmap[t..], 0x0a);
             ll += 1;
         }
-        let foo = fast_read_xy(&mmap[t..]);
+        let foo = fast_read_xy(&mmap[t..]).unwrap();
         println!(
-            "{} {}..={} ({}..={}, {}, {}, {}) {} {}",
-            a,
-            v[a].start_ix,
-            v[a].end_ix,
-            v[a].start_line,
-            v[a].end_line,
-            v[a].line_starts.len(),
-            v[a].end_line - v[a].start_line,
-            v[a].line_valid.len(),
-            foo.x,
-            foo.y
+            "{} {}..={} ({}..={}) {} {}",
+            a, v[a].start_ix, v[a].end_ix, v[a].start_line, v[a].end_line, foo.x, foo.y
         )
     }
 
@@ -341,12 +392,23 @@ fn main() {
     let n_comments: usize = v.par_iter_mut().map(|a| a.invalidate_comments()).sum();
     println!("{} comment lines found\n", n_comments);
 
-    let _dummy = v
+    let og_pair: Vec<(usize,Vec<usize>)> = v
         .par_iter()
-        .map(|a| a.sharded_read(&xys_under_rwlock, sharding_prime));
+        .map(|a| a.sharded_read(&xys_under_rwlock, sharding_prime))
+        .collect();
+    let og: (usize,usize) = og_pair.iter().fold((0,0), |(sx,sy),(x,y)| {(sx+x,sy+y.len())});
+    println!("{} non-comment lines read for sharding, {} bad\n", og.0, og.1);
+
+    for a in 0..n_chunks
+    {
+      for b in &(og_pair[a].1)
+      {
+        v[a].line_valid.set(*b,false);
+      }
+    }
 
     println!(
-        "Chunk 0 of sharded read has {} entries",
+        "Chunk 0 of sharded read has {} entries (expect about {})",
         xys_under_rwlock
             .read()
             .unwrap()
@@ -354,16 +416,18 @@ fn main() {
             .unwrap()
             .lock()
             .unwrap()
-            .len()
+            .len(),
+        og.0 / n_shards
     );
 
     // Now that we've done the part requiring aggressive locking,
     // can we make the thing read-only for the part where we do the labelling,
     // counting and output?
 
-    let n_duplicates: usize = v.par_iter_mut()
+    let n_duplicates: usize = v
+        .par_iter_mut()
         .map(|a| a.mark_dupes(&xys_under_rwlock, sharding_prime))
         .sum();
     println!("{} duplicates found", n_duplicates);
-    emit_uncancelled_lines(args.outfn, &v, &mmap).unwrap();
+    emit_uncancelled_lines(args.outfn, &v).unwrap();
 }
